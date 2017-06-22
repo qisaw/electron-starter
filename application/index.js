5bd8bfda120fa99a0af9360e2716eb866658503b
Wasiq Kashkari <w.kashkari@outlook.com>
2017-06-23 09:56:40 +1200

    add state to demo hot reload

--- application/index.js
+++ application/index.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './app.css';
 
-const App = () => <div className={styles.hello}>what there</div>;
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      val: 0,
+    }
+    this.increment = this.increment.bind(this);
+  }
+  increment() {
+    const { val } = this.state;
+    this.setState({ val: val + 1 });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment}>do something</button>
+        {this.state.val}
+      </div>
+    )
+  }
+}
 
 export default App;
